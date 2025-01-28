a20ec2f22c49d4988a5ded51c3098d6a1fdb9cca
John van Arkelen <john@johnvanarkelen.nl>
2025-01-28 11:16:41 +0100

    updated user loader

--- app/services/dwh/loaders/users_loader.rb
+++ app/services/dwh/loaders/users_loader.rb
@@ -9,10 +9,27 @@ class Dwh::Loaders::UsersLoader
       users.each do |user|
         dim_company = Dwh::DimCompany.find_by(account_id: dim_account.id, original_id: user.data['company_id'])
         unless dim_company.blank?
-          Dwh::DimUser.upsert({ account_id: dim_account.id, original_id: user.data['original_id'], full_name: user.data['full_name'], company_id: dim_company.id, 
-            start_date: user.data['start_date'], leave_date: user.data['leave_date'], role: user.data['role'], email: user.data['email'], employee_type: user.data['employee_type'], 
-            contract: user.data['contract'], contract_hours: user.data['contract_hours'], salary: user.data['salary'], address: user.data['address'],
-            zipcode: user.data['zipcode'], city: user.data['city'], country: user.data['country']}, unique_by: [:account_id, :original_id])
+          # Find the user. If not found, create it, otherwise update it
+          # If an existing user now has a different company, create a new user
+          existing_user = Dw::DimUser.find_by(account_id: dim_account.id, original_id: user.data['original_id'])
+          if existing_user.blank?
+            Dw::DimUser.create({ account_id: dim_account.id, original_id: user.data['original_id'], full_name: user.data['full_name'], company_id: dim_company.id, 
+              start_date: user.data['start_date'], leave_date: user.data['leave_date'], role: user.data['role'], email: user.data['email'], employee_type: user.data['employee_type'], 
+              contract: user.data['contract'], contract_hours: user.data['contract_hours'], salary: user.data['salary'], address: user.data['address'],
+              zipcode: user.data['zipcode'], city: user.data['city'], country: user.data['country']})
+          else
+            if existing_user.company_id != dim_company.id
+              Dw::DimUser.create({ account_id: dim_account.id, original_id: user.data['original_id'], full_name: user.data['full_name'], company_id: dim_company.id, 
+                start_date: user.data['start_date'], leave_date: user.data['leave_date'], role: user.data['role'], email: user.data['email'], employee_type: user.data['employee_type'], 
+                contract: user.data['contract'], contract_hours: user.data['contract_hours'], salary: user.data['salary'], address: user.data['address'],
+                zipcode: user.data['zipcode'], city: user.data['city'], country: user.data['country']})
+            else
+              existing_user.update({ account_id: dim_account.id, original_id: user.data['original_id'], full_name: user.data['full_name'], company_id: dim_company.id, 
+                start_date: user.data['start_date'], leave_date: user.data['leave_date'], role: user.data['role'], email: user.data['email'], employee_type: user.data['employee_type'], 
+                contract: user.data['contract'], contract_hours: user.data['contract_hours'], salary: user.data['salary'], address: user.data['address'],
+                zipcode: user.data['zipcode'], city: user.data['city'], country: user.data['country']})
+            end
+          end
         end
         user.destroy
       end
