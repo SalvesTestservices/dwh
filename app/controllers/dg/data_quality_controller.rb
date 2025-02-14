25abe4e49abf30d76c24bf7313b15035235c6306
John van Arkelen <john@johnvanarkelen.nl>
2025-02-14 13:06:34 +0100

    added read for dg quality log

--- app/controllers/dg/data_quality_controller.rb
+++ app/controllers/dg/data_quality_controller.rb
@@ -1,4 +1,6 @@
 class Dg::DataQualityController < ApplicationController
+  include ActionView::Helpers::TagHelper
+
   before_action :authenticate_user!
   before_action -> { authorize!(:read, :data_governance) }, only: [:index]
 
@@ -9,8 +11,21 @@ class Dg::DataQualityController < ApplicationController
     dg_quality_log = Dwh::DgQualityLog.find(params[:data_quality_id])
     dg_quality_log.update(read_at: Time.current)
     
-    render turbo_stream: [
-      turbo_stream.remove(dg_quality_log)
-    ]
+    remaining_logs = Dwh::DgQualityLog.where(read_at: nil)
+    
+    if remaining_logs.empty?
+      render turbo_stream: [
+        turbo_stream.update('data_quality_logs_list', 
+          content_tag(:li, class: 'flex items-center justify-center px-3 py-6') do
+            content_tag(:div, I18n.t('.data_quality.messages.no_logs'), 
+                       class: 'text-base leading-6 text-gray-900')
+          end
+        )
+      ]
+    else
+      render turbo_stream: [
+        turbo_stream.remove(dg_quality_log)
+      ]
+    end
   end
 end
